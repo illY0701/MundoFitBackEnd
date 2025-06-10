1a0f16ea07a2c3af10632775a9a8bf52d5baf5c0
Anna Isabelle <145376029+nekaevi@users.noreply.github.com>
2025-06-10 16:05:10 -0300

    privatekey alterada

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,19 +1,28 @@
-// utils/firebase.js
-
 const admin = require('firebase-admin');
-require('dotenv').config(); // Carrega variáveis do .env se existir
+require('dotenv').config();
 
 let serviceAccount;
 
-if (process.env.FIREBASE_CONFIG) {
-  // Carrega o JSON das credenciais da variável de ambiente
-  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+if (process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_CLIENT_EMAIL) {
+  serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    //private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: "googleapis.com"
+  };
 } else {
-  // Fallback para ambiente local usando o arquivo .json
   serviceAccount = require('../serviceAccountKey.json');
 }
 
-// Inicialização do Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
